89cefb3de4a867032379ac439492c32fb5ed35d3
Morteza <m.ziaeemehr@gmail.com>
2026-05-04 00:16:06 +0200

    fix: import main.css in entry point to enable Tailwind

--- apps/client/src/main.ts
+++ apps/client/src/main.ts
@@ -3,6 +3,7 @@ import { createPinia } from "pinia";
 import { VueQueryPlugin } from "@tanstack/vue-query";
 import { router } from "./router/index.js";
 import App from "./app/App.vue";
+import "./styles/main.css";
 
 const app = createApp(App);
 app.use(createPinia());
